3205ddccebacccd61ae82b19b7a24413d913bc77
Черезов Артем <avcherezov@gmail.com>
2020-08-26 21:49:08 +0300

    final version 1.0

---
+++ yatube/posts/tests.py
@@ -0,0 +1,213 @@
+from django.test import TestCase, Client, override_settings
+from django.contrib.auth import get_user_model
+from .models import Post, Group, User, Follow
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files.images import ImageFile
+from django.core.cache import cache
+
+
+User = get_user_model()
+
+
+class ProfileTest(TestCase):
+    
+    def setUp(self):
+        """
+        Регистрация пользователя и создание новой записи
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(username='sarah')
+        self.post = Post.objects.create(text="test1", author=self.user)
+    
+    def test_profile(self):
+        """
+        Проверка - после регистрации пользователя 
+        создается его персональная страница (profile)
+        """
+        self.client.force_login(self.user)
+        response = self.client.get('/sarah/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_new(self):
+        """
+        Проверка - авторизованный пользователь 
+        может опубликовать пост (new)
+        """
+        self.client.force_login(self.user)
+        response = self.client.post("/new/", {"text": self.post.text}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        """
+        Проверка - неавторизованный посетитель не может 
+        опубликовать пост и его редиректит на страницу входа
+        """
+        response = self.client.get('/new/')
+        self.assertRedirects(response, '/auth/login/?next=/new/')
+
+    def test_new_entry(self):
+        """
+        Проверка - после публикации поста новая запись 
+        появляется на главной странице сайта (index), 
+        на персональной странице пользователя (profile), 
+        и на отдельной странице поста (post)
+        """
+        self.client.force_login(self.user)
+        response = self.client.get('/')
+        self.assertContains(response, 'test1')
+        response = self.client.get('/sarah/')
+        self.assertContains(response, 'test1')
+        response = self.client.get('/sarah/1/')
+        self.assertContains(response, 'test1')
+
+    @override_settings(CACHES=settings.TEST_CACHES)
+    def test_user_edit_post(self):
+        """
+        Проверка - авторизованный пользователь может 
+        отредактировать свой пост и его содержимое 
+        изменится на всех связанных страницах
+        """
+        self.client.force_login(self.user)
+        new_text = 'Test'
+        self.client.post('/sarah/1/edit', {'text': new_text})
+        response = self.client.get('/')
+        self.assertContains(response, new_text)
+        response = self.client.get('/sarah/')
+        self.assertContains(response, new_text)
+        response = self.client.get('/sarah/1/')
+        self.assertContains(response, new_text)
+
+    def test_not_found(self):
+        """
+        Проверка - возвращает сервер код 404,
+        если страница не найдена
+        """
+        response = self.client.get("/user/1/")
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get("/user/")
+        self.assertEqual(response.status_code, 404)
+
+
+class ImageTest(TestCase):
+    
+    def setUp(self):
+        """
+        Регистрация пользователя, создание новой группы 
+        и создание новой записи с картинкой
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(username='sarah')
+        self.group = Group.objects.create(title='foo', slug='foo', description='foo')
+        self.post = Post.objects.create(text="test1", author=self.user)
+        small_gif = (
+                b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04'
+                b'\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02'
+                b'\x02\x4c\x01\x00\x3b'
+        )
+        image = SimpleUploadedFile('small.gif', small_gif, content_type='image/gif')
+        self.client.force_login(self.user)
+        with ImageFile(image) as fp:
+            self.client.post('/sarah/1/edit', {'text': self.post.text, 'group': self.group.id, 'image': fp})
+
+    def test_image1(self):
+        """
+        Проверяем страницу конкретной записи с картинкой: 
+        на странице есть тег <img>
+        """
+        self.client.force_login(self.user)
+        response = self.client.get('/sarah/1/')
+        self.assertContains(response, 'img')
+
+    def test_image2(self):
+        """
+        Проверяем, что на главной странице, на странице профайла 
+        и на странице группы пост с картинкой отображается 
+        корректно, с тегом <img>
+        """
+        response = self.client.get('/')
+        self.assertContains(response, 'img')
+        response = self.client.get('/sarah/')
+        self.assertContains(response, 'img')
+        response = self.client.get('/group/foo/')
+        self.assertContains(response, 'img')
+
+    def test_image3(self):
+        """
+        Проверяем, что срабатывает защита от загрузки файлов 
+        не-графических форматов
+        """
+        self.post = Post.objects.create(text="test2", author=self.user)
+        self.client.force_login(self.user)
+        with open('manage.py', 'rb') as fp:
+            self.client.post('/sarah/2/edit', {'text': self.post.text, 'image': fp})
+        response = self.client.get('/sarah/2/')
+        self.assertNotContains(response, 'img')
+
+    def test_cache(self):
+        """
+        Проверяем работу кэша
+        """
+        response = self.client.get('/')
+        self.post = Post.objects.create(text="test2", author=self.user)
+        response = self.client.get('/')
+        self.assertNotContains(response, "test2")
+        cache.clear()
+        response = self.client.get('/')
+        self.assertContains(response, "test2")
+
+
+class FollowTest(TestCase):
+
+    def setUp(self):
+        """
+        Регистрация пользователей и создание новых записей
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(username='sarah')
+        self.user2 = User.objects.create_user(username='conor')
+        self.user3 = User.objects.create_user(username='arni')
+        self.post = Post.objects.create(text="test1", author=self.user)
+        self.post = Post.objects.create(text="test2", author=self.user2)
+        self.post = Post.objects.create(text="test3", author=self.user3)
+
+    def test_follow1(self):
+        """
+        Авторизованный пользователь может подписываться 
+        на других пользователей и удалять их из подписок
+        """
+        self.client.force_login(self.user)
+        follow = Follow.objects.create(user=self.user, author=self.user2)
+        response = self.client.get('/follow/')
+        self.assertContains(response, "test2")
+        follow.delete()
+        response = self.client.get('/follow/')
+        self.assertNotContains(response, "test2")
+
+    def test_follow2(self):
+        """
+        Новая запись пользователя появляется в ленте тех, 
+        кто на него подписан и не появляется в ленте тех, 
+        кто не подписан на него
+        """
+        self.client.force_login(self.user)
+        follow = Follow.objects.create(user=self.user, author=self.user2)
+        self.client.force_login(self.user2)
+        self.post = Post.objects.create(text="test4", author=self.user2)
+        self.client.force_login(self.user)
+        response = self.client.get('/follow/')
+        self.assertContains(response, "test4")
+        self.client.force_login(self.user3)
+        response = self.client.get('/follow/')
+        self.assertNotContains(response, "test4")
+
+    def test_follow3(self):
+        """
+        Неавторизированный пользователь не может комментировать посты 
+        и его редиректит на страницу входа
+        """
+        response = self.client.get('/sarah/1/comment/')
+        self.assertRedirects(response, '/auth/login/?next=/sarah/1/comment/')
+
+
+
---
+++ yatube/posts/views.py
@@ -0,0 +1,139 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Group, User, Comment, Follow
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+
+def index(request):
+    post_list = Post.objects.order_by("-pub_date").all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, 'index.html', {'page': page, 'paginator': paginator})
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = Post.objects.filter(group=group).order_by("-pub_date").all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, "group.html", {"group": group, 'page': page, 'paginator': paginator})
+ 
+@login_required
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+    return render(request, "post_new.html", {"form": form})
+
+def profile(request, username):
+    post_list = Post.objects.filter(author__username=username).order_by("-pub_date").all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page') 
+    page = paginator.get_page(page_number)
+    count_post = Post.objects.filter(author__username=username).count()
+    author = get_object_or_404(User, username=username)
+    if request.user.is_authenticated:
+        following = Follow.objects.filter(user=request.user).filter(author=author)
+        return render(request, "profile.html", {'page': page, 'paginator': paginator, 'count_post': count_post, 'author': author, 'following': following})
+    else:
+        return render(request, "profile.html", {'page': page, 'paginator': paginator, 'count_post': count_post, 'author': author})
+
+
+def post_view(request, username, post_id):
+    author = get_object_or_404(User, username=username)
+    post = get_object_or_404(Post, pk=post_id, author=author)
+    count_post = Post.objects.filter(author__username=username).count()
+    items = Comment.objects.filter(post=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = author
+            comment.save()
+            return redirect('post', username, post_id)
+    else:
+        form = CommentForm()
+    return render(request, "post.html", {'author': author, 'post': post, 'count_post': count_post, 'items': items, "form": form})
+
+def post_edit(request, username, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author == request.user:
+        if request.method == 'POST':
+            form = PostForm(request.POST or None, files=request.FILES or None, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('post', username, post_id)
+        else:
+            form = PostForm(instance=post)
+        return render(request, "post_new.html", {"form": form, "post": post})
+    else:
+        return redirect('post', username, post_id)
+
+def page_not_found(request, exception):
+        return render(request, "misc/404.html", {"path": request.path}, status=404)
+
+def server_error(request):
+        return render(request, "misc/500.html", status=500)
+
+@login_required
+def add_comment(request, username, post_id):
+    author = get_object_or_404(User, username=username)
+    post = get_object_or_404(Post, pk=post_id, author=author)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = author
+            comment.save()
+            return redirect('post', username, post_id)
+    else:
+        form = CommentForm()
+    return render(request, "comments.html", {"form": form})
+
+@login_required
+def follow_index(request):
+    follow = Follow.objects.filter(user=request.user)
+    authors = []
+    for obj in follow:
+        follow_author = obj.author
+        authors.append(follow_author)
+    post_list = Post.objects.filter(author__in=authors).order_by("-pub_date").all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, "follow.html", {'page': page, 'paginator': paginator})
+
+@login_required
+def profile_follow(request, username):
+    user = get_object_or_404(User, username=username)
+    author = get_object_or_404(User, username=username)
+    count = Follow.objects.filter(user=request.user).filter(author=author).count()
+    if request.user != user:
+        if count == 0:
+            Follow.objects.create(user=request.user, author=author)
+            return redirect('profile', username)
+        else:
+            return redirect('profile', username)
+    else:
+        return redirect('profile', username)
+
+@login_required
+def profile_unfollow(request, username):
+    author = get_object_or_404(User, username=username)
+    follow = Follow.objects.filter(user=request.user).filter(author=author)
+    follow.delete()
+    return redirect('profile', username)
+
+
